c63dab6cdfa350acbab69d5294e3618f40a7da7a
不知道叫什么好 <2921855939@qq.com>
2024-06-17 21:57:34 +0800

    feat: 添加funny english

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -30,6 +30,10 @@ export default defineConfig({
         link: "/composition",
       },
       {
+        text: "Funny English",
+        link: "/funny/",
+      },
+      {
         text: "Study Way",
         link: "/study_way",
       },
@@ -49,6 +53,19 @@ export default defineConfig({
             { text: "2-2 我喜欢打羽毛球", link: "/level2/2" },
           ],
         },
+      ],
+      "/funny/": [
+        { text: "社恐", link: "/funny/1" },
+        { text: "社牛", link: "/funny/2" },
+        { text: "缩写", link: "/funny/3" },
+        { text: "惊呆了", link: "/funny/4" },
+        { text: "always 与进行时连用的不同含义", link: "/funny/5" },
+        { text: "有趣的完全重叠词", link: "/funny/6" },
+        { text: "短语句型替换", link: "/funny/7" },
+        { text: "你必须知道的地道表达", link: "/funny/8" },
+        { text: "用食物来形容一个人", link: "/funny/9" },
+        { text: "写作中常用的万能短语", link: "/funny/10" },
+        { text: "常见疑难易错点分析积累", link: "/funny/11" },
       ],
       "/composition/": [
         {

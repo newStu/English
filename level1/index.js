2529f0d0a6e4376097dd585f394484b4240c7c23
不知道叫什么好 <2921855939@qq.com>
2024-06-18 22:41:24 +0800

    feat: 添加路由

--- level1/index.js
+++ level1/index.js
@@ -1,6 +1,7 @@
 export default [
   {
     text: "Level 1",
+    link: "/level1/",
     items: [],
   },
 ];
--- level2/index.js
+++ level2/index.js
@@ -1,6 +1,7 @@
 export default [
   {
     text: "Level 2",
+    link: "/level2/",
     items: [
       { text: "2-1 我想吃披萨", link: "/level2/1" },
       { text: "2-2 我喜欢打羽毛球", link: "/level2/2" },

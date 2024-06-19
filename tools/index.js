6babc314e3810182c0e9b1b3ad52a0e610b7ab1f
不知道叫什么好 <2921855939@qq.com>
2024-06-19 22:12:05 +0800

    feat: 开始自动写入目录

--- tools/index.js
+++ tools/index.js
@@ -1,2 +1,38 @@
-console.log(import.meta.url);
+import * as path from "path";
+import * as fs from "fs";
+import * as globby from "globby";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function loadFile(filePath) {
+  if (!fs.existsSync(filePath)) {
+    return null;
+  }
+
+  return fs.readFileSync(filePath, "utf-8");
+}
+
+export function getAllMdFile() {
+  // new URL("../funny", import.meta.url).pathname.substring(1)
+  const baseDir = path.resolve(__dirname, "../funny");
+  // 获取所有文件路径
+  const filePaths = globby.globbySync("**/*.md", {
+    cwd: baseDir,
+  });
+
+  filePaths.forEach((filePath) => {
+    const fullPath = path.join(baseDir, filePath);
+
+    if (!fs.statSync(fullPath).isFile()) return;
+    // 读取文件
+    const detail = loadFile(fullPath);
+
+    console.log(detail, "--------");
+  });
+}
+
+getAllMdFile();
+
 console.log(new URL("../funny", import.meta.url).href);

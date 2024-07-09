60b214616940e9d39b252174b4b9fa5b10ebef28
zzbaby <38250793+1011764719@users.noreply.github.com>
2024-07-09 16:54:39 +0800

    音变笔记提交
    音变笔记提交  --

--- pronunciation/index.js
+++ pronunciation/index.js
@@ -1,24 +1,24 @@
 export default [
   {
-    "text": "",
-    "items": [
-      {
-        "text": "音变",
-        "link": "/pronunciation/0_transformation.md"
-      }
-    ]
+    text: "美式音标",
+    items: [],
   },
   {
-    "text": "美式音标",
-    "items": []
+    text: "",
+    items: [
+      {
+        text: "国际音标",
+        link: "/pronunciation/international_phonetic_alphabet/0.md",
+      },
+    ],
   },
   {
-    "text": "",
-    "items": [
+    text: "",
+    items: [
       {
-        "text": "国际音标",
-        "link": "/pronunciation/international_phonetic_alphabet/0.md"
-      }
-    ]
-  }
-]
+        text: "音变",
+        link: "/pronunciation/0_transformation.md",
+      },
+    ],
+  },
+];

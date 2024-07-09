7ad23a4c4a4ec16d856d25745cb81d165dea7adb
zzbaby <38250793+1011764719@users.noreply.github.com>
2024-07-09 14:40:07 +0800

    邀请函的笔记提交
    邀请函的笔记提交--1

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vitepress";
 import funny from "../funny/index";
 import composition from "../composition/index";
@@ -7,79 +6,6 @@ import level2 from "../level2/index";
 import pronunciation from "../pronunciation/index";
 import grammar from "../grammar/index";
 import readwrite from "../readwrite/index";
-
-export default defineConfig({
-  // 站点级选项
-  title: "English Study",
-  description: "English Lessons Notes",
-  base: "/English/",
-  srcDir: ".",
-  outDir: "dist",
-  head: [["link", { rel: "icon", href: "/English/icon.svg" }]],
-  themeConfig: {
-    lastUpdated: { text: "2024-06-16" },
-    outlineTitle: "页面导航",
-    logo: "/icon.svg",
-    socialLinks: [
-      { icon: "github", link: "https://github.com/newStu/English" },
-    ],
-    search: {
-      provider: "local",
-    },
-    nav: [
-      { text: "Lesson", link: "/lesson" },
-      { text: "Level 1", link: "/level1/" },
-      { text: "Level 2", link: "/level2/" },
-      // { text: "Level 3", link: "/level3/" },
-      // { text: "Level 4", link: "/level4/" },
-      // { text: "Level 5", link: "/level5/" },
-      {
-        text: "Composition",
-        link: "/composition",
-      },
-      {
-        text: "Funny English",
-        link: "/funny/",
-      },
-      {
-        text: "Pronunciation",
-        link: "/pronunciation/",
-      },
-      {
-        text: "Grammar",
-        link: "/grammar/",
-      },
-      {
-        text: "Read Write",
-        link: "/readwrite/",
-      },
-      {
-        text: "Study Way",
-        link: "/study_way",
-      },
-    ],
-    sidebar: {
-      "/level1/": level1,
-      "/level2/": level2,
-      "/funny/": funny,
-      "/composition/": composition,
-      "/pronunciation/": pronunciation,
-      "/grammar/": grammar,
-      "/readwrite/": readwrite,
-    },
-    footer: {
-      message: "Released under the MIT License.",
-      copyright: "Copyright © 2024-present WW And PP",
-    },
-  },
-});
-=======
-import { defineConfig } from "vitepress";
-import funny from "../funny/index";
-import composition from "../composition/index";
-import level1 from "../level1/index";
-import level2 from "../level2/index";
-import pronunciation from "../pronunciation/index";
 import education from "../education/index";
 
 export default defineConfig({
@@ -108,16 +34,24 @@ export default defineConfig({
       // { text: "Level 4", link: "/level4/" },
       // { text: "Level 5", link: "/level5/" },
       {
-        text: "Composition",
-        link: "/composition",
+        text: "Grammar",
+        link: "/grammar/",
+      },
+      {
+        text: "Read Write",
+        link: "/readwrite/",
+      },
+      {
+        text: "Pronunciation",
+        link: "/pronunciation/",
       },
       {
         text: "Funny English",
         link: "/funny/",
       },
       {
-        text: "Pronunciation",
-        link: "/pronunciation/",
+        text: "Composition",
+        link: "/composition",
       },
       {
         text: "Education",
@@ -127,9 +61,11 @@ export default defineConfig({
     sidebar: {
       "/level1/": level1,
       "/level2/": level2,
+      "/grammar/": grammar,
+      "/readwrite/": readwrite,
+      "/pronunciation/": pronunciation,
       "/funny/": funny,
       "/composition/": composition,
-      "/pronunciation/": pronunciation,
       "/education/": education,
     },
     footer: {
@@ -138,4 +74,3 @@ export default defineConfig({
     },
   },
 });
->>>>>>> b08fa4517346733590a4074b4b35eaa992acaa43
--- pronunciation/index.js
+++ pronunciation/index.js
@@ -1,25 +1,24 @@
 export default [
-<<<<<<< HEAD
-	{ text: "音变", link: "/pronunciation/0_transformation.md" },
-	{ text: "美式音标", link: "/pronunciation/American_phonetic_alphabet/0.md" },
-	{ text: "国际音标", link: "/pronunciation/international_phonetic_alphabet/0.md" },
-=======
+  {
+    "text": "",
+    "items": [
+      {
+        "text": "音变",
+        "link": "/pronunciation/0_transformation.md"
+      }
+    ]
+  },
   {
     "text": "美式音标",
     "items": []
   },
   {
-    "text": "国际音标",
+    "text": "",
     "items": [
       {
-        "text": "元音",
-        "link": "/pronunciation/International_phonetic_alphabet/1.md"
-      },
-      {
-        "text": "辅音",
-        "link": "/pronunciation/International_phonetic_alphabet/2.md"
+        "text": "国际音标",
+        "link": "/pronunciation/international_phonetic_alphabet/0.md"
       }
     ]
   }
->>>>>>> b08fa4517346733590a4074b4b35eaa992acaa43
 ]

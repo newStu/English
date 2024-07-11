8f88f1027446f1c131772c6675e4c266f2bcfeb8
zzbaby <38250793+1011764719@users.noreply.github.com>
2024-07-11 10:36:05 +0800

    剑桥词典图标配置-

--- pronunciation/index.js
+++ pronunciation/index.js
@@ -1,24 +1,24 @@
 export default [
   {
-    text: "美式音标",
-    items: [],
+    "text": "",
+    "items": [
+      {
+        "text": "音变",
+        "link": "/pronunciation/0_transformation.md"
+      }
+    ]
   },
   {
-    text: "",
-    items: [
-      {
-        text: "国际音标",
-        link: "/pronunciation/international_phonetic_alphabet/0.md",
-      },
-    ],
+    "text": "美式音标",
+    "items": []
   },
   {
-    text: "",
-    items: [
+    "text": "",
+    "items": [
       {
-        text: "音变",
-        link: "/pronunciation/0_transformation.md",
-      },
-    ],
-  },
-];
+        "text": "国际音标",
+        "link": "/pronunciation/international_phonetic_alphabet/0.md"
+      }
+    ]
+  }
+]

55adee371de4565d46336a3033a8e6679db7c02c
不知道叫什么好 <2921855939@qq.com>
2025-05-20 21:46:54 +0800

    修改img的路径错误导致Build失败

--- level2/index.js
+++ level2/index.js
@@ -1,72 +1,68 @@
 export default [
   {
-    text: "懂你 A+",
-    items: [
+    "text": "懂你 A+",
+    "items": [
       {
-        text: "单词",
-        link: "/level2/A_class/2-0.md",
+        "text": "单词",
+        "link": "/level2/A_class/2-0.md"
       },
       {
-        text: "2-1 我想吃披萨",
-        link: "/level2/A_class/2-1.md",
+        "text": "2-1 我想吃披萨",
+        "link": "/level2/A_class/2-1.md"
       },
       {
-        text: "2-10 让我们给她办一个派对",
-        link: "/level2/A_class/2-10.md",
+        "text": "2-10 让我们给她办一个派对",
+        "link": "/level2/A_class/2-10.md"
       },
       {
-        text: "2-2 我喜欢打羽毛球",
-        link: "/level2/A_class/2-2.md",
+        "text": "2-2 我喜欢打羽毛球",
+        "link": "/level2/A_class/2-2.md"
       },
       {
-        text: "2-3 喜好与观点",
-        link: "/level2/A_class/2-3.md",
+        "text": "2-3 喜好与观点",
+        "link": "/level2/A_class/2-3.md"
       },
       {
-        text: "2-4 我喜欢大多数的零食",
-        link: "/level2/A_class/2-4.md",
+        "text": "2-4 我喜欢大多数的零食",
+        "link": "/level2/A_class/2-4.md"
       },
       {
-        text: "2-5 现在的交通很拥挤",
-        link: "/level2/A_class/2-5.md",
+        "text": "2-5 现在的交通很拥挤",
+        "link": "/level2/A_class/2-5.md"
       },
       {
-        text: "2-6 我出生在多伦多",
-        link: "/level2/A_class/2-6.md",
+        "text": "2-6 我出生在多伦多",
+        "link": "/level2/A_class/2-6.md"
       },
       {
-        text: "2-7 我想去钓鱼",
-        link: "/level2/A_class/2-7.md",
+        "text": "2-7 我想去钓鱼",
+        "link": "/level2/A_class/2-7.md"
       },
       {
-        text: "2-8 我的预算是 200 美元",
-        link: "/level2/A_class/2-8.md",
+        "text": "2-8 我的预算是 200 美元",
+        "link": "/level2/A_class/2-8.md"
       },
       {
-        text: "2-9 我准备找一个新工作",
-        link: "/level2/A_class/2-9.md",
-      },
-      {
-        text: "2-10 让我们给她办一个派对",
-        link: "/level2/A_class/2-10.md",
-      },
-    ],
+        "text": "2-9 我准备找一个新工作",
+        "link": "/level2/A_class/2-9.md"
+      }
+    ]
   },
   {
-    text: "教学课",
-    items: [
+    "text": "教学课",
+    "items": [
       {
-        text: "unit 1",
-        link: "/level2/teach_class/unit1.md",
+        "text": "unit 1",
+        "link": "/level2/teach_class/unit1.md"
       },
       {
-        text: "unit 2",
-        link: "/level2/teach_class/unit2.md",
+        "text": "unit 2",
+        "link": "/level2/teach_class/unit2.md"
       },
       {
-        text: "unit 3",
-        link: "/level2/teach_class/unit3.md",
-      },
-    ],
-  },
-];
+        "text": "unit 3",
+        "link": "/level2/teach_class/unit3.md"
+      }
+    ]
+  }
+]

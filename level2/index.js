93478f1e2dacca6b72ea14bc555cc47111401f69
不知道叫什么好 <2921855939@qq.com>
2025-06-25 22:59:48 +0800

    语法-句型-笔记整理提交

--- level2/index.js
+++ level2/index.js
@@ -1,128 +1,148 @@
 export default [
   {
-    text: "懂你 A+",
-    items: [
+    "text": "懂你 A+",
+    "items": [
       {
-        text: "单词",
-        link: "/level2/A_class/2-00.md",
+        "text": "单词",
+        "link": "/level2/A_class/2-00.md"
       },
       {
-        text: "2-01 我想吃披萨",
-        link: "/level2/A_class/2-01.md",
+        "text": "2-01 我想吃披萨",
+        "link": "/level2/A_class/2-01.md"
       },
       {
-        text: "2-02 我喜欢打羽毛球",
-        link: "/level2/A_class/2-02.md",
+        "text": "2-02 我喜欢打羽毛球",
+        "link": "/level2/A_class/2-02.md"
       },
       {
-        text: "2-03 喜好与观点",
-        link: "/level2/A_class/2-03.md",
+        "text": "2-03 喜好与观点",
+        "link": "/level2/A_class/2-03.md"
       },
       {
-        text: "2-04 我喜欢大多数的零食",
-        link: "/level2/A_class/2-04.md",
+        "text": "2-04 我喜欢大多数的零食",
+        "link": "/level2/A_class/2-04.md"
       },
       {
-        text: "2-05 现在的交通很拥挤",
-        link: "/level2/A_class/2-05.md",
+        "text": "2-05 现在的交通很拥挤",
+        "link": "/level2/A_class/2-05.md"
       },
       {
-        text: "2-06 我出生在多伦多",
-        link: "/level2/A_class/2-06.md",
+        "text": "2-06 我出生在多伦多",
+        "link": "/level2/A_class/2-06.md"
       },
       {
-        text: "2-07 我想去钓鱼",
-        link: "/level2/A_class/2-07.md",
+        "text": "2-07 我想去钓鱼",
+        "link": "/level2/A_class/2-07.md"
       },
       {
-        text: "2-08 我的预算是 200 美元",
-        link: "/level2/A_class/2-08.md",
+        "text": "2-08 我的预算是 200 美元",
+        "link": "/level2/A_class/2-08.md"
       },
       {
-        text: "2-09 我准备找一个新工作",
-        link: "/level2/A_class/2-09.md",
+        "text": "2-09 我准备找一个新工作",
+        "link": "/level2/A_class/2-09.md"
       },
       {
-        text: "2-10 让我们给她办一个派对",
-        link: "/level2/A_class/2-10.md",
+        "text": "2-10 让我们给她办一个派对",
+        "link": "/level2/A_class/2-10.md"
       },
       {
-        text: "2-11 你想来参加聚会吗?",
-        link: "/level2/A_class/2-11.md",
+        "text": "2-11 你想来参加聚会吗?",
+        "link": "/level2/A_class/2-11.md"
       },
       {
-        text: "2-12 让我带你参观办公室",
-        link: "/level2/A_class/2-12.md",
+        "text": "2-12 让我带你参观办公室",
+        "link": "/level2/A_class/2-12.md"
       },
       {
-        text: "2-13 每年有数百万人来这里",
-        link: "/level2/A_class/2-13.md",
+        "text": "2-13 每年有数百万人来这里",
+        "link": "/level2/A_class/2-13.md"
       },
       {
-        text: "2-14 今天有可能下雨",
-        link: "/level2/A_class/2-14.md",
+        "text": "2-14 今天有可能下雨",
+        "link": "/level2/A_class/2-14.md"
       },
       {
-        text: "2-15 这家酒店更新",
-        link: "/level2/A_class/2-15.md",
+        "text": "2-15 这家酒店更新",
+        "link": "/level2/A_class/2-15.md"
       },
       {
-        text: "2-16 伦敦比纽约更古老",
-        link: "/level2/A_class/2-16.md",
+        "text": "2-16 伦敦比纽约更古老",
+        "link": "/level2/A_class/2-16.md"
       },
       {
-        text: "2-17 我想换更便宜的公寓",
-        link: "/level2/A_class/2-17.md",
+        "text": "2-17 我想换更便宜的公寓",
+        "link": "/level2/A_class/2-17.md"
       },
       {
-        text: "2-18 我喜欢我的新工作",
-        link: "/level2/A_class/2-18.md",
+        "text": "2-18 我喜欢我的新工作",
+        "link": "/level2/A_class/2-18.md"
       },
       {
-        text: "2-19 去超市买东西",
-        link: "/level2/A_class/2-19.md",
+        "text": "2-19 去超市买东西",
+        "link": "/level2/A_class/2-19.md"
       },
       {
-        text: "2-20 热身后再运动",
-        link: "/level2/A_class/2-20.md",
+        "text": "2-20 热身后再运动",
+        "link": "/level2/A_class/2-20.md"
       },
       {
-        text: "2-21 学会和团队合作",
-        link: "/level2/A_class/2-21.md",
+        "text": "2-21 学会和团队合作",
+        "link": "/level2/A_class/2-21.md"
       },
       {
-        text: "2-22 我发生了车祸",
-        link: "/level2/A_class/2-22.md",
+        "text": "2-22 我发生了车祸",
+        "link": "/level2/A_class/2-22.md"
       },
       {
-        text: "2-23 我的头很痛",
-        link: "/level2/A_class/2-23.md",
+        "text": "2-23 我的头很痛",
+        "link": "/level2/A_class/2-23.md"
       },
       {
-        text: "2-24 我没能订到其他的航班",
-        link: "/level2/A_class/2-24.md",
+        "text": "2-24 我没能订到其他的航班",
+        "link": "/level2/A_class/2-24.md"
       },
       {
-        text: "2-25 你们必须按时上课",
-        link: "/level2/A_class/2-25.md",
+        "text": "2-25 你们必须按时上课",
+        "link": "/level2/A_class/2-25.md"
       },
-    ],
+      {
+        "text": "2-26 你必须休息",
+        "link": "/level2/A_class/2-26.md"
+      },
+      {
+        "text": "2-27 你应该去看埃菲尔铁塔",
+        "link": "/level2/A_class/2-27.md"
+      },
+      {
+        "text": "2-28 你坐过地铁吗?",
+        "link": "/level2/A_class/2-28.md"
+      },
+      {
+        "text": "2-29 我当导游已经十年",
+        "link": "/level2/A_class/2-29.md"
+      },
+      {
+        "text": "2-30 礼品店以前就在这里",
+        "link": "/level2/A_class/2-30.md"
+      }
+    ]
   },
   {
-    text: "教学课",
-    items: [
+    "text": "教学课",
+    "items": [
       {
-        text: "unit 1",
-        link: "/level2/teach_class/unit1.md",
+        "text": "unit 1",
+        "link": "/level2/teach_class/unit1.md"
       },
       {
-        text: "unit 2",
-        link: "/level2/teach_class/unit2.md",
+        "text": "unit 2",
+        "link": "/level2/teach_class/unit2.md"
       },
       {
-        text: "unit 3",
-        link: "/level2/teach_class/unit3.md",
-      },
-    ],
-  },
-];
+        "text": "unit 3",
+        "link": "/level2/teach_class/unit3.md"
+      }
+    ]
+  }
+]

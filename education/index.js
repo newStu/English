91afb5daaf017f672dde079e6fa2396855664ba3
zzbaby <1011764719@qq.com>
2025-12-14 22:29:40 +0800

    -1

---
+++ education/index.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    "text": "习惯",
+    "items": [
+      {
+        "text": "幼儿教育",
+        "link": "/education/habit/1.md"
+      },
+      {
+        "text": "完善自己，保护孩子的一生",
+        "link": "/education/habit/2.md"
+      }
+    ]
+  },
+  {
+    "text": "方法",
+    "items": [
+      {
+        "text": "如何成为学习高手 ？",
+        "link": "/education/way/1.md"
+      }
+    ]
+  }
+]
---
+++ pronunciation/index.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    "text": "",
+    "items": [
+      {
+        "text": "音变",
+        "link": "/pronunciation/音变.md"
+      },
+      {
+        "text": "音变",
+        "link": "/pronunciation/音变2.md"
+      }
+    ]
+  },
+  {
+    "text": "",
+    "items": [
+      {
+        "text": "国际音标",
+        "link": "/pronunciation/IPA/0.md"
+      }
+    ]
+  },
+  {
+    "text": "",
+    "items": [
+      {
+        "text": "美式音标",
+        "link": "/pronunciation/KKA/0.md"
+      },
+      {
+        "text": "音标区分",
+        "link": "/pronunciation/KKA/音标区分.md"
+      }
+    ]
+  }
+]
